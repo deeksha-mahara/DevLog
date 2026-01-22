04db1a40fb7a09a331a7a2f027eada8d5fb9659b
Deeksha <deekshamahara95@gmail.com>
2026-01-22 11:26:54 +0530

    upgraded ui and added commenting system

--- index.js
+++ index.js
@@ -2,25 +2,36 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const Blog = require("./models/blog");
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express();
 const PORT = 8000;
-mongoose.connect("mongodb://127.0.0.1:27017/blogify")
-    .then(() => console.log("MongoDB Connected"))
-    .catch((err) => console.log("Mongo Error", err));
-    
-app.use(express.urlencoded({ extended: false })); 
-app.use(cookieParser()); 
-app.use(express.static(path.resolve("./public"))); 
-app.use("/user", userRoute);
-
-app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
-
+mongoose
+  .connect("mongodb://127.0.0.1:27017/blogify")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Mongo Error", err));
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.use(express.urlencoded({ extended: false })); 
+app.use(cookieParser()); 
+app.use(checkForAuthenticationCookie("token")); 
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
 });
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
--- models/blog.js
+++ models/blog.js
@@ -1,29 +1,24 @@
-const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
 
-const userRoute = require("./routes/user");
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    coverImageURL: {
+        type: String,
+        required: false,
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",  
+    }
+}, { timestamps: true });
 
-const app = express();
-const PORT = 8000;
+const Blog = mongoose.model("blog", blogSchema);
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/blogify")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("Mongo Error", err));
-
-app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
-
-app.use(express.urlencoded({ extended: false })); 
-app.use(cookieParser()); 
-app.use(express.static(path.resolve("./public")));
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.use("/user", userRoute); 
-
-app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
+module.exports = Blog;
